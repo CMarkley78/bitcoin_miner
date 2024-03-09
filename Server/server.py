b8f93df908d9847ed5c917142cf62d974981167f
DrRed <calebmarkley626@gmail.com>
2024-03-09 12:59:07 -0500

    Update server.py

--- Server/server.py
+++ Server/server.py
@@ -2,11 +2,9 @@ import socket
 import binascii
 import hashlib
 from bitcoinrpc.authproxy import AuthServiceProxy
-from bitcoinlib.transactions import Transaction, Input, Output
-from bitcoinlib.networks import Network
-from random import randint
+from random import randint, choice
 from time import time
-from bitcoinutils.bits import difficulty_to_target, target_to_bits
+
 
 print ("Server started.")
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -16,18 +14,15 @@ server_socket.bind(server_address)
 server_socket.listen(10)
 
 def create_work():
-    return bytes.fromhex("0100000000000000000000000000000000000000000000000000000000000000000000003BA3EDFD7A7B12B27AC72C3E67768F617FC81BC3888A51323A9FB8AA4B1E5E4A29AB5F49FFFF001D")+bytes.fromhex("00000001")
+    return bytes.fromhex("01000000")+bytes.fromhex("0000000000000000000000000000000000000000000000000000000000000000")+bytes.fromhex("".join([choice([x for x in "0123456789ABCDEF"]) for _ in range(64)]))+bytes.fromhex("29AB5F495DBA0317")+bytes.fromhex("00000001")
+    return bytes.fromhex("01000000")+bytes.fromhex("0000000000000000000000000000000000000000000000000000000000000000")+bytes.fromhex("".join([choice([x for x in "0123456789ABCDEF"]) for _ in range(64)]))+bytes.fromhex("29AB5F49FFFF001D")+bytes.fromhex("00000001")
+    return bytes.fromhex("01000000")+bytes.fromhex("0000000000000000000000000000000000000000000000000000000000000000")+bytes.fromhex("3BA3EDFD7A7B12B27AC72C3E67768F617FC81BC3888A51323A9FB8AA4B1E5E4A")+bytes.fromhex("29AB5F49FFFF001D")+bytes.fromhex("00000001")
 
 def difficulty_to_nbits(difficulty):
-    # Calculate exponent
-    exponent = 0
-    while (difficulty // 256**exponent) >= 256:
-        exponent += 1
+    difficulty = float(difficulty)
+    hex_str = "00ffff"+("0"*26)
+    return hex(int(float(int(hex_str,16))/difficulty))
 
-    mantissa = difficulty // 256**exponent
-
-    nbits = (exponent << 24) | mantissa
-    return nbits
 
 def hashIt(firstTxHash, secondTxHash):
     unhex_reverse_first = binascii.unhexlify(firstTxHash)[::-1]
@@ -58,32 +53,13 @@ def create_work_2():
     #Add prevhash
     header = header+bytes.fromhex(rpc_connection.getbestblockhash())
 
-    #Merkle Root comes next, so we need to generate coinbase and bring other txs to verify
-    coinbase_tx = Transaction(coinbase=True,network="testnet")
-    coinbase_tx.inputs.append(Input(bytes([0 for _ in range(32)]),int(6.25 * 10**8),script=b'Solo Mined by Caleb Markley', sequence=0xffffffff))
-    reward_output = Output(6.25 * 10**8, "tb1qwsa28pt43ntyng6zy6adzzhyx03jucvyxf3qja",network="testnet")
-    coinbase_tx.outputs.append(reward_output)
-
-    txdat = [coinbase_tx.raw().hex()]
-    txids = [coinbase_tx.txid]
-
-    for txid in rpc_connection.getrawmempool()[:9]:
-        txids.append(txid)
-        txdat.append(rpc_connection.getrawtransaction(txid))
-
-    header = header + merkleCalculator(txids)
-
-    header = header + int(time()).to_bytes(4,"little")
-
-    max_target = 0xffff * 2**(8 * (0x1d - 3))
-    target = max_target/rpc_connection.getdifficulty()
-    hex_target = hex(int(target))[2:]
-    main_part = hex_target[:6]
+    print (difficulty_to_nbits(rpc_connection.getdifficulty()))
 
 
 
 
-while False:
+
+while True:
     print ("\nWaiting for client to send a message...")
     connection, client_address = server_socket.accept()
     data = connection.recv(1)
